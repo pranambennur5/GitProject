8a6d1454f0ae1b1ebb6ce17c4c09513ad675a87a
pranambennur5 <pranambennur@gmail.com>
2022-05-12 20:11:13 -0500

    for loop

--- python.py
+++ python.py
@@ -1,9 +1,6 @@
 #Practice Python and become expert in it
-<<<<<<< HEAD
-print("Pranam")
-=======
-#You are doing great. Keep going. 
-
 
 print("Hello Pranam")
->>>>>>> 6a9506faa71d877fdaf83346b71e8bb0ea7d8838
+
+for i in range(6):
+ print(i)
